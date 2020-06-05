6302f752ec4e4859a9ce5d0c31fae02154178896
arendach <arendach.taras@gmail.com>
2020-06-05 03:24:42 +0300

    fix seeder

--- database/seeds/ProductHistorySeeder.php
+++ database/seeds/ProductHistorySeeder.php
@@ -8,10 +8,16 @@ class ProductHistorySeeder extends Seeder
     public function run()
     {
         DB::connection('old')->table('history_product')->get()->each(function (stdClass $item) {
+            try {
+                $data = json_decode(htmlspecialchars_decode($item->data));
+            } catch (Exception $exception) {
+                return null;
+            }
+
             ProductHistory::create([
                 'product_id' => $item->product,
                 'type'       => $item->type,
-                'data'       => htmlspecialchars_decode($item->data),
+                'data'       => $data,
                 'user_id'    => $item->author,
                 'created_at' => $item->date
             ]);
