e62ca396e116de82f477233306b0966112a4ddea
brendi211 <s5660max@gmail.com>
2021-02-11 23:45:16 +0200

    FIX current shops orders

--- resources/views/shop/orders/main.blade.php
+++ resources/views/shop/orders/main.blade.php
@@ -14,7 +14,10 @@
         <tr>
             @foreach($sites as $site)
                 <td>
-                    @if($_REQUEST['shop'] != $site->key)
+                    @php
+                    $shop = ($_REQUEST['shop']) ?? 'shop';
+                    @endphp
+                    @if($shop != $site->key)
                     <a style="font-size: 18px" href="{{ uri('shop/orders/main?shop='.$site->key) }}">{{ $site->name }}</a>
                     @else
                         <span style="font-size: 18px">{{ $site->name }} (Текущий)</span>
